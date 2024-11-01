8b815fc5420cbd0fa7fd67d1ac42d85c88780922
veaquer <veaquer@gmail.com>
2024-11-01 11:06:07 +0100

    fixed docs error

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,19 @@
 //! The idea is to convert everything in one query, like text, units, just whatever.
 //! Here are magic_convert function that can convert units and translate text.
 //! ## Example of usage `magic_convert`:
-//! ```
-//! use convers::convert::magic_convert;
 //!
-//! #[tokio::main]
-//! async fn main() {
-//! 	 let query = "1m to cm".to_string();
-//! 	 let result = magic_convert(&query).await.unwrap();
-//! 	 let translate_result = magic_convert(&"en:de how are u?".to_string()).await.unwrap();
-//! 	 println!("{}", result); // prints translated text
-//! 	 println!("{}", result); // prints "100.0 Centimeter"
-//! }
-
+//! use convers::convert::magic_convert;
+//! use tokio::runtime::Runtime;
+//!
+//! let query = "1m to cm".to_string();
+//! let translate_query = "en:de how are u?".to_string();
+//!
+//! let rt = Runtime::new().unwrap();
+//! let result = rt.block_on(async { magic_convert(&query).await }).unwrap();
+//! let translate_result = rt.block_on(async { magic_convert(&translate_query).await }).unwrap();
+//!
+//! println!("{}", result); // prints "100.0 Centimeter"
+//! println!("{}", translate_result); // prints translated text
+//! ```
 pub mod convert;
 pub mod utils;
