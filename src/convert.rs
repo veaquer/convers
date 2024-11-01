6b50115703fd0fff8c24a4841ae085122d8cf71d
veaquer <veaquer@gmail.com>
2024-11-01 09:51:29 +0100

    added some docs

--- src/convert.rs
+++ src/convert.rs
@@ -4,8 +4,8 @@ use crate::utils::{
 };
 
 /// This is magic function that's used to convert units and translate text
-/// Use that **format** for translate 'lg to lg text' or 'lg:lg text' where `lg` is language code.
-/// Use that format for convert 'nu to u' or 'nu:u' where 'n' is number and 'u' is unit.
+/// Use that **format** for translate `lg to lg text` or `lg:lg text` where `lg` is language code.
+/// Use that **format** for convert `nu to u` or `nu:u` where `n` is number and `u` is unit.
 pub async fn magic_convert(query: &String) -> Result<String, TranslatorError> {
     let measure_response = Measurement::convert(query);
     let tr = Translator::new();
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,20 @@
+//! # convers
+//!
+//! `convers` is cool convert library.
+//! The idea is to convert everything in one query, like text, units, just whatever.
+//! Here are magic_convert function that can convert units and translate text.
+//! ## Example of usage `magic_convert`:
+//! ```
+//! use convers::convert::magic_convert;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//! 	 let query = "1m to cm".to_string();
+//! 	 let result = magic_convert(&query).await.unwrap();
+//! 	 let translate_result = magic_convert(&"en:de how are u?".to_string()).await.unwrap();
+//! 	 println!("{}", result); // prints translated text
+//! 	 println!("{}", result); // prints "100.0 Centimeter"
+//! }
+
 pub mod convert;
 pub mod utils;
